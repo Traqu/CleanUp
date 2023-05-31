8aee098d890e96816ad32fcb4911ceaf5795458e
TraquPjatk <s19787@pjwstk.edu.pl>
2023-05-31 20:18:58 +0200

    Added removed files size log and executables

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
         final String dayzLocalPath = System.getProperty("user.home") + "\\AppData\\Local\\DayZ";
         File file = new File(dayzLocalPath);
         File[] files = file.listFiles();
+        long size = 0;
 
         try {
             for (File content : files) {
                 if (!content.isDirectory()) {
+                    size += Files.size(content.toPath());
                     if (content.delete())
                         System.out.println("Removed: " + content.getName());
                 }
             }
         } catch (NullPointerException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        double totalSizeInMB = (double) size / (1024 * 1024);
+        DecimalFormat df = new DecimalFormat("#.##");
+        String formattedTotalSize = df.format(totalSizeInMB);
+        System.out.println("\nRemoved: " + formattedTotalSize + " MB of data.");
     }
 }
