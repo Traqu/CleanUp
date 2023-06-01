2b29df32a824cbf2d135165263d2116a1ccb606d
TraquPjatk <s19787@pjwstk.edu.pl>
2023-06-01 20:15:55 +0200

    Added appIcon and title

--- src/Gui.java
+++ src/Gui.java
@@ -8,7 +8,7 @@ public class Gui extends JFrame {
     private static int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
     private static int screenHeigth = Toolkit.getDefaultToolkit().getScreenSize().height;
     private Font font = new Font("Baghdad", Font.BOLD, 12);
-    private JTextField jTextField = new JTextField("TEXT");
+    private JTextField jTextField = new JTextField();
     public Gui() throws HeadlessException {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(width, heigth);
@@ -18,15 +18,17 @@ public class Gui extends JFrame {
                 screenWidth / 2 - this.getPreferredSize().width / 2 - width / 2,
                 screenHeigth / 2 - this.getPreferredSize().height / 2
         );
+        this.setTitle("     Created by Traqu");
+
+
         this.add(jTextField);
         jTextField.setEditable(false);
         jTextField.setHorizontalAlignment((int) CENTER_ALIGNMENT);
         jTextField.setFont(font);
 
-//        String[] fontFamilies = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-//        for (String fontFamily : fontFamilies) {
-//            System.out.println(fontFamily);
-//        }
+
+        ImageIcon logo = new ImageIcon("resources/icons/logo.png");
+        this.setIconImage(logo.getImage());
     }
 
     public JTextField getjTextField() {
