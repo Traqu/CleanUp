02007bcd94f2370b8ac5eb59089c2593c7b6b99c
TraquPjatk <s19787@pjwstk.edu.pl>
2023-06-02 16:53:17 +0200

    GUI is now more informative

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,36 @@ public class Main {
         final String dayzLocalPath = System.getProperty("user.home") + "\\AppData\\Local\\DayZ";
         File file = new File(dayzLocalPath);
         File[] files = file.listFiles();
+        boolean anythingRemoved = false;
         long size = 0;
         DecimalFormat df = new DecimalFormat("#.##");
         DecimalFormat dfTiny = new DecimalFormat("#.###");
-        double tmpSIZE;
+        double removedSize;
+
+        long tmpTotalSize = 0;
+        double totalSize = 0;
+
+        gui.getjTextField().setText("Counting total files size");
 
         try {
+            for (File fileSize : files) {
+                if (!fileSize.isDirectory()) {
+                    tmpTotalSize += Files.size(fileSize.toPath());
+                    totalSize = (double) tmpTotalSize / (1024 * 1024);
+                }
+            }
             for (File content : files) {
                 if (!content.isDirectory()) {
                     size += Files.size(content.toPath());
-                    if (content.delete())
-                        System.out.println("Removed: " + content.getName());
-                    tmpSIZE = (double) size / (1024 * 1024);
+                    if (content.delete()) {
+//                        System.out.println("Removed: " + content.getName());
+                        anythingRemoved = true;
+                    }
+                    removedSize = (double) size / (1024 * 1024);
                     if (size < 10000) {
-                        gui.getjTextField().setText("Removed: " + dfTiny.format(tmpSIZE) + " MB of data.");
+                        gui.getjTextField().setText("Removed: " + dfTiny.format(removedSize) + "/" + dfTiny.format(totalSize) + " MB of data.");
                     } else {
-                        gui.getjTextField().setText("Removed: " + df.format(tmpSIZE) + " MB of data.");
+                        gui.getjTextField().setText("Removed: " + df.format(removedSize) + "/" + df.format(totalSize) + " MB of data.");
                     }
                 }
             }
@@ -34,9 +48,13 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-        if (size == 0)
-            gui.getjTextField().setText("Nothing was removed");
-
+        if (size == 0) {
+            if (anythingRemoved) {
+                gui.getjTextField().setText("Only empty files were removed");
+            } else {
+                gui.getjTextField().setText("Nothing was removed");
+            }
+        }
 
         try {
             Thread.sleep(2200);
